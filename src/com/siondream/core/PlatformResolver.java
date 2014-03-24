c4d0edb2f08a7da7c5af888190e6e3f4dba76c65
David Saltares <david.saltares@gmail.com>
2014-03-24 13:21:17 +0000

    Removes listFolder() from PlatformResolver

--- src/com/siondream/core/PlatformResolver.java
+++ src/com/siondream/core/PlatformResolver.java
@@ -1,10 +1,7 @@
 package com.siondream.core;
 
-import com.badlogic.gdx.files.FileHandle;
-
 public interface PlatformResolver {
 	public void openURL(String url);
 	public void rateApp();
 	public void sendFeedback();
-	public FileHandle[] listFolder(String path);
 }
